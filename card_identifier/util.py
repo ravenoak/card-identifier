bc1576e1d2bb26c2d343e47ccb0406b8cc4400bf
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-06-09 23:22:21 -0700

    Fix HTTP error retry and add test

--- card_identifier/util.py
+++ card_identifier/util.py
@@ -24,12 +24,14 @@ def setup_logging(debug: bool = False):
 def retry_if_http_error(e: Exception):
     if isinstance(e, HTTPError):
         logger.error(f"HTTP error: {e.code} {e.url}")
-        return HTTPError.code == 429
+        return e.code == 429
 
 
-@retry(retry_on_exception=retry_if_http_error,
-       wait_exponential_multiplier=100,
-       wait_exponential_max=10000)
+@retry(
+    retry_on_exception=retry_if_http_error,
+    wait_exponential_multiplier=100,
+    wait_exponential_max=10000,
+)
 def download_save_image(url: str, path: pathlib.Path) -> bool:
     image = requests.get(url, allow_redirects=True)
     logger.debug(f"downloaded image: {url}")
